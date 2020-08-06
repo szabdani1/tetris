41a3885d58b15e4f5acf3412481791ef7ff5a4d9
szabdani1 <34173432+szabdani1@users.noreply.github.com>
2020-08-06 13:05:47 +0200

    Working game

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Tetris from './components/Tetris'
 const App = () =>(
   <div className="App">
-    <Tetris></Tetris>
+    <Tetris/>
   </div>
 
 );
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import {StyledCell} from './styles/StyledCell'
-import {TETROMINOS} from '../tetrominos'
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
 
-const Cell = ({ type }) =>(
-    <StyledCell type={type} color={TETROMINOS[type].color}/>
-)
-export default Cell;
+// React.memo makes sure we only re-render the changed cells
+const Cell = ({ type }) => (
+  <StyledCell type={type} color={TETROMINOS[type].color}>
+    {console.log('rerender cell')}
+  </StyledCell>
+);
+
+export default React.memo(Cell);
